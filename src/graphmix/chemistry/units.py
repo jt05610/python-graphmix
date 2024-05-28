6fa43d9ccf8bfd166147fb6a45706821860f57b0
jtaylor <jonrtaylor12@gmail.com>
2024-05-28 00:42:41 -0600

    unit dimensionality enforcement

--- src/graphmix/chemistry/units.py
+++ src/graphmix/chemistry/units.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+from enum import StrEnum
 from typing import Annotated
 from typing import Any
 
@@ -13,63 +15,101 @@ ureg.setup_matplotlib()
 ureg.default_format = "P~"
 
 
-class _QuantityPydanticAnnotation:
-    @classmethod
-    def __get_pydantic_core_schema__(
-        cls, _source_type: Any, _handler: GetCoreSchemaHandler
-    ) -> CoreSchema:
-        """
-        Returns a CoreSchema with the following behavior:
-        * ints or floats will be converted to ureg.Quantity with no unit.
-        * strings will be converted to ureg.Quantity with a unit if specified in the string.
-        * ureg.Quantity instances will be parsed as is.
-        * Nothing else will pass validation.
-        * Serialization will always return a string.
-        """
+def dimensionality_validator(
+    dimensionality: str | None = None, default_unit: str | None = None
+):
 
-        def validate(value: str) -> ureg.Quantity:
+    if dimensionality is None:
+
+        def validator_func(value: ureg.Quantity) -> ureg.Quantity:
+            return value
+
+    else:
+
+        def validator_func(value: ureg.Quantity) -> ureg.Quantity:
+            if value.dimensionality != dimensionality:
+                raise ValueError(
+                    f"Expected a quantity with dimensionality {dimensionality}, but got {value.dimensionality}."
+                )
+            return value
+
+    if default_unit is None:
+
+        def convert_func(value: int | float | str | Decimal) -> ureg.Quantity:
             return ureg.Quantity(value)
 
-        from_number_schema = core_schema.chain_schema(
-            [
-                core_schema.union_schema(
+    else:
+
+        def convert_func(value: int | float | str | Decimal) -> ureg.Quantity:
+            return ureg.Quantity(value, default_unit)
+
+    class _QuantityPydanticAnnotation:
+
+        @classmethod
+        def __get_pydantic_core_schema__(
+            cls, _source_type: Any, _handler: GetCoreSchemaHandler
+        ) -> CoreSchema:
+            """
+            Returns a CoreSchema with the following behavior:
+            * ints or floats will be converted to ureg.Quantity with no unit.
+            * strings will be converted to ureg.Quantity with a unit if specified in the string.
+            * ureg.Quantity instances will be parsed as is.
+            * Nothing else will pass validation.
+            * Serialization will always return a string.
+            """
+
+            from_number_schema = core_schema.chain_schema(
+                [
+                    core_schema.union_schema(
+                        [
+                            core_schema.int_schema(),
+                            core_schema.float_schema(),
+                            core_schema.str_schema(),
+                        ]
+                    ),
+                    core_schema.no_info_plain_validator_function(convert_func),
+                    core_schema.no_info_plain_validator_function(
+                        validator_func
+                    ),
+                ]
+            )
+
+            return core_schema.json_or_python_schema(
+                json_schema=from_number_schema,
+                python_schema=core_schema.union_schema(
                     [
-                        core_schema.int_schema(),
-                        core_schema.float_schema(),
-                        core_schema.str_schema(),
+                        core_schema.chain_schema(
+                            [
+                                core_schema.is_instance_schema(ureg.Quantity),
+                                core_schema.no_info_plain_validator_function(
+                                    validator_func
+                                ),
+                            ]
+                        ),
+                        from_number_schema,
                     ]
                 ),
-                core_schema.no_info_plain_validator_function(validate),
-            ]
-        )
-
-        return core_schema.json_or_python_schema(
-            json_schema=from_number_schema,
-            python_schema=core_schema.union_schema(
-                [
-                    core_schema.is_instance_schema(ureg.Quantity),
-                    from_number_schema,
-                ]
-            ),
-            serialization=core_schema.plain_serializer_function_ser_schema(
-                lambda instance: str(instance)
-            ),
-        )
-
-    @classmethod
-    def __get_pydantic_json_schema(
-        cls, _core_schema: CoreSchema, handler: GetJsonSchemaHandler
-    ) -> JsonSchemaValue:
-        return handler(
-            core_schema.union_schema(
-                core_schema.int_schema(),
-                core_schema.float_schema(),
-                core_schema.str_schema(),
+                serialization=core_schema.plain_serializer_function_ser_schema(
+                    lambda instance: str(instance)
+                ),
             )
-        )
+
+        @classmethod
+        def __get_pydantic_json_schema(
+            cls, _core_schema: CoreSchema, handler: GetJsonSchemaHandler
+        ) -> JsonSchemaValue:
+            return handler(
+                core_schema.union_schema(
+                    core_schema.int_schema(),
+                    core_schema.float_schema(),
+                    core_schema.str_schema(),
+                )
+            )
+
+    return _QuantityPydanticAnnotation
 
 
-Quantity = Annotated[ureg.Quantity, _QuantityPydanticAnnotation]
+Quantity = Annotated[ureg.Quantity, dimensionality_validator()]
 
 Q_ = Quantity
 
@@ -85,3 +125,32 @@ mmol = ureg.mmol
 mM = ureg.mM
 percent = ureg.percent
 g = ureg.g
+
+
+class Dimensionality(StrEnum):
+    Mass = "[mass]"
+    Substance = "[substance]"
+    MolarMass = "[mass] / [substance]"
+    MolarConcentration = "[substance] / [length] ** 3"
+    MassConcentration = "[mass] / [length] ** 3"
+    Volume = "[length] ** 3"
+    Percent = "dimensionless"
+
+
+class DimQuantity:
+
+    def __class_getitem__(
+        cls, item: str | Dimensionality, default_unit: str | None = None
+    ):
+        return Annotated[
+            ureg.Quantity, dimensionality_validator(item, default_unit)
+        ]
+
+
+Mass = DimQuantity[Dimensionality.Mass]
+Substance = DimQuantity[Dimensionality.Substance]
+MolarMass = DimQuantity[Dimensionality.MolarMass]
+MolarConcentration = DimQuantity[Dimensionality.MolarConcentration]
+MassConcentration = DimQuantity[Dimensionality.MassConcentration]
+Volume = DimQuantity[Dimensionality.Volume]
+Percent = DimQuantity[Dimensionality.Percent]
--- tests/chemical/unit/test_units.py
+++ tests/chemical/unit/test_units.py
@@ -1,6 +1,12 @@
+import itertools
+from typing import Iterable
+
+import pytest
 from pydantic import BaseModel
+from pydantic import ValidationError
 
 from graphmix.chemistry.units import Q_
+from graphmix.chemistry.units import Volume
 
 
 def test_quantity_works_with_pydantic():
@@ -20,3 +26,47 @@ def test_quantity_works_with_pydantic():
 
     for m, v in checks_to_run:
         assert_model(m, v)
+
+
+def iter_tuple_with_arguments(t: Iterable[tuple], length: int) -> tuple:
+    """Iterates through an iterable of tuples. If there are less than `length`
+    elements in the tuple, it will fill the rest with `None` values.
+    """
+
+    for args in t:
+        yield tuple(
+            itertools.chain(args, itertools.repeat(None, length - len(args)))
+        )
+
+
+def test_quantity_with_enforced_dimensionality():
+    class FakeModel(BaseModel):
+        volume: Volume
+
+    def assert_model(
+        passed_value: Q_ | str | float,
+        value: Q_,
+        expected_error: Exception | None = None,
+    ):
+        def assertions(inst: FakeModel):
+            assert inst.volume == value
+            assert inst.model_dump() == {"volume": f"{value:#~}"}
+
+        if expected_error is not None:
+            with pytest.raises(expected_error):
+                assertions(FakeModel(volume=passed_value))
+            return
+        instance = FakeModel(volume=passed_value)
+        assertions(instance)
+
+    checks_to_run = (
+        (Q_("100 uL"), Q_("100 uL")),
+        (Q_(100, "uL"), Q_("100 uL")),
+        (100, Q_("100 uL"), ValidationError),
+        ("100 uL", Q_("100 uL")),
+        ("100 s", Q_("100 uL"), ValidationError),
+        (Q_(100, "s"), Q_("100 uL"), ValidationError),
+    )
+
+    for m, v, e in iter_tuple_with_arguments(checks_to_run, 3):
+        assert_model(m, v, e)
